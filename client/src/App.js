c07ee033265324a59bbc078a24eceda198bb72da
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2020-12-12 03:24:21 +0530

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        Image Steganalysis
+      </div>
+    </div>
+  );
+}
+
+export default App;
